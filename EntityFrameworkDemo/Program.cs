37b1fa05e2939eaa6c98020d60e6770f3d89da89
Eness <Eness@ENES>
2022-05-21 22:20:45 +0300

    Proje dosyası ekle.

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            // GetAll(northwindContext);
+            GetProductsByCategory(1);
+
+        }
+
+        private static void GetAll(NorthwindContext northwindContext)
+        {
+            foreach (var item in northwindContext.Products)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            var result = northwindContext.Products.Where(p => p.CategoryId == categoryId);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
